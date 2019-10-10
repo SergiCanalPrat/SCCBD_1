413ee702b24c650f01c41f9c7f52c9b9d20d32d7
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2019-10-10 08:33:54 +0200

    cambios

--- frontend/src/app/services/main.service.ts
+++ frontend/src/app/services/main.service.ts
@@ -9,10 +9,10 @@ export class MainService {
   constructor(private http: HttpClient) { }
   get(mens: Object) {
     console.log("recibo", mens)
-    return this.http.get('http://localhost:3000/get' + `/${mens}`, mens);
-  }
-  post(mens: Object) {
-    console.log("envio", mens)
-    return this.http.post('http://localhost:3000/post' + `/${mens}`, mens);
+  return this.http.get('http://localhost:3000/get'+  `/${mens}` , mens);
+  } 
+  post(mens: Object){
+    //console.log("envio", mens)
+    return this.http.post('http://localhost:3000/post' +  `/${mens}` , mens);
   }
 }
