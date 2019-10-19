9915df9f97d0b6ec9ed1defe8fdab4701dcd40fd
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2019-10-19 16:45:05 +0200

    get arreglado

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@
 const express = require ('express');
 const logger = require ('morgan');
 const cors = require('cors');
+//const cipher = require('ci')
 const crypto = require('crypto');
 const app = express();
 
@@ -33,16 +34,16 @@ app.use(cors());
 
 //FUNCIONES POST, GET, ENCRIPTAR, DESENCRIPTAR
 
-const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY; // Must be 256 bits (32 characters)
+const KEY_LENGTH = 32; 
 const IV_LENGTH = 16; // For AES, this is always 16
 let iv = crypto.randomBytes(IV_LENGTH);
-let key = Buffer.from(ENCRYPTION_KEY);
+let key = crypto.randomBytes(KEY_LENGTH);
 
 app.post( '/post/:mns',	(req, res) => {
 	let mns = req.params.mns;
 	console.log('este mensaje recibo del frontend: '+ mns);
 	let enmns = encrypt(mns);
-	res.json (enmns)
+	res.json (enmns);
 }) 
 
 app.get('/get/:mns', (req,res) => {
@@ -50,25 +51,25 @@ app.get('/get/:mns', (req,res) => {
 	console.log('este mensaje recibo del server: '+ emns);
 	let demns = decrypt(emns);
 	console.log('este mensaje recibo del server y lo desencripto: '+ demns);
-	res.json (demns)
+	res.json (demns);
 })
 
 //funcion de encriptar
 function encrypt (msg){
-	console.log('encrypt del server 1')	
-	let encrypted = crypto.createCipher('aes-256-cbc', key,iv);
-	let cipher = cipher.update(msg);
-	cipher = Buffer.concat([encrypted, cipher.final()]);
-	return {cipherData: cipher.toString('hex')};
+	console.log('encrypt del server 1');
+	let cipher = crypto.createCipher('aes-256-cbc', key,iv);
+	let encrypted = cipher.update(msg);
+	encrypted = Buffer.concat([encrypted, cipher.final()]);
+	return {encryptedData: encrypted.toString('hex')};
 	}
 
 //funcion de encriptar
 function decrypt (msg){
-	console.log('decrypt del server 1')
+	console.log('decrypt del server 1');
 	let descrypted = crypto.createDecipher('aes-256-cbc',key,iv);
-	let decipher = decipher.update(msg);
+	let decipher = descrypted.update(msg);
 	decipher = Buffer.concat([descrypted, decipher.final()]);
-	return {decipher: decipher.toString('hex')}
+	return {decipher: decipher.toString('hex')};
 }
 
 
