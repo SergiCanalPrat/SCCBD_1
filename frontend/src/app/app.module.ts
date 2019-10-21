49f93c27ea9a090d425514844ff8a64bf0f6455c
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2019-10-21 11:43:39 +0200

    camnbio nombre carpeta

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './componets/main/main.component';
+import { MainComponent } from './components/main/main.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
