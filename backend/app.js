3a8c037954205678a42c31b71c3d462ccd7f78c6
SergiCanalPrat <sergi.canal.prat@estudiant.upc.edu>
2019-12-12 10:43:36 +0100

    ya puedes trabajar

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,20 @@ const app = express();
 const bigintCryptoUtils = require('bigint-crypto-utils');
 const moment = require('moment')
 const jwt = require('jwt-simple')
+const moneyInBank = require('./modelos/cuenta')
+const mongoose = require('mongoose')
 
 
 app.use(logger('dev')); // Log requests (GET..)
 app.use(express.json()); // Needed to retrieve JSON
+
+mongoose.connect(config.db, (err, res) => {
+	if (err) {return console.log(`Error al conectar a la base de datos: ${err}`)}
+	console.log('Conexión a la base de datos establecida...')
+	server = app.listen(config.port, (err, res) => {
+	  console.log(`API REST corriendo en http://localhost:${config.port}`)
+	})
+
 
 //conexion al puerto
 const PORT = process.env.port || 3000;
@@ -57,20 +67,8 @@ let dfront;
 
 
 
-//Funciones del pryecto
-
-
-
+//Funciones del proyecto
 app.post('/login/:name',(req,res) => {
-	let value = req.params.value;
-	let moneyBlind = req.params.pass;
-	console.log('cuerpo PostMoney ',value, moneyBlind)
-	encryptRSA(value, moneyBlind)
-	return res.json(token)
-		  
-})
-
-app.post('/postMoney/:name',(req,res) => {
 	let name = req.params.name;
 	let password = req.params.pass;
 	console.log('usuario logeandose ',name, password)
@@ -79,39 +77,14 @@ app.post('/postMoney/:name',(req,res) => {
 		  
 })
 
-
-<<<<<<< HEAD
-//funiones de ENTREGAS
-console.log (key);
- app.get('/getiv', (req,res) => {
-	//res.json (buf2hex(iv));
-	res.json(d.toString(16));
- })
-
-app.get('/getkey', (req,res) => {
-	//res.json (buf2hex(key));	
-	res.json(n.toString(16));
-	
+app.post('/postMoney/:value',(req,res) => {
+	let value = req.params.value;
+	let moneyBlind = req.params.pass;
+	console.log('cuerpo PostMoney', value, moneyBlind)
+	moneyInBank = moneyInBank - value; //El banco resta de la cuenta del cliente el valor de la moneda
+	let signMoney = signMoney(value, moneyBlind) //El banco firma la moneda
+	return res.json(signMoney)
 })
-||||||| merged common ancestors
-
-
-//funiones de ENTREGAS
-console.log (key);
- app.get('/getiv', (req,res) => {
-	//res.json (buf2hex(iv));
-	res.json(d.toString(16));
- })
-
-app.get('/getkey', (req,res) => {
-	//res.json (buf2hex(key));	
-	res.json(n.toString(16));
-	
-})
-=======
-
-
->>>>>>> 7d4fbf9f0297abcf80eb33ab2f1f4819bf2251e6
 
 
 //FUNCIONES DEL PROYECTO
@@ -128,7 +101,8 @@ function createToken(user) {
 
 function buf2hex(buffer) { // buffer is an ArrayBuffer
 	return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-  }
+}
+
 function hex2ab2(hex){
 	var typedArray = new Uint8Array(hex.match(/[\da-f]{2}/gi).map(function(h){
 	  return parseInt(h, 16)
@@ -136,19 +110,22 @@ function hex2ab2(hex){
 	var buffer = typedArray.buffer
 	return buffer
   }
+
 function d2h(d) {
 	return d.toString(16);
-  }
+}
+
 function ascii_to_hexa(str)
-  {
+{
 	var arr1 = [];
 	for (var n = 0, l = str.length; n < l; n ++)
-     {
+		{
 		var hex = Number(str.charCodeAt(n)).toString(16);
 		arr1.push(hex);
-	 }
+		}
 	return arr1.join('');
-   }
+}
+
 //funcion para crear key RSA
 async function KeyRSA5(){
 	console.log('Voy a crear la Key de 5')
@@ -190,24 +167,24 @@ async function KeyRSA20(){
 
 
 //funcion para encriptar RSA
-function encryptRSA(msg, value){
+function signMoney(msg, value){
 	let msgbuf = Buffer.from(msg,'utf8');
 	let msgbig = BigInt('0x' + msgbuf.toString('hex'));
-	let cryptoRSA;
+	let signMoney;
 	if (value == 5){
-		cryptoRSA = bigintCryptoUtils.modPow(msgbig,e5,n5)
+		signMoney = bigintCryptoUtils.modPow(msgbig,e5,n5)
 	}
 	else if (value == 10){
-		cryptoRSA = bigintCryptoUtils.modPow(msgbig,e10,n10)
+		signMoney = bigintCryptoUtils.modPow(msgbig,e10,n10)
 	}
 	else if (value == 20){
-		cryptoRSA = bigintCryptoUtils.modPow(msgbig,e20,n20)
+		signMoney = bigintCryptoUtils.modPow(msgbig,e20,n20)
 	}
 	else {
-		cryptoRSA = 'Valor incorrecto' 
+		signMoney = 'Valor incorrecto' 
 	}
-	console.log('3', cryptoRSA)
-	return cryptoRSA;
+	console.log('3', signMoney)
+	return signMoney;
 }
 
 
@@ -226,3 +203,6 @@ function decryptRSA(msg, d, n){
 	console.log('1', decryptedRSA)
 	return decryptedRSA;
 }
+
+})
+
